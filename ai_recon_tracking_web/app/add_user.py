ff38c76ed878ea4c024269ce31b8ea096b74b65d
Bhanuja Sharma <bhanuja1602@gmail.com>
2025-07-29 18:52:53 +0530

    Update add_user.py

--- ai_recon_tracking_web/app/add_user.py
+++ ai_recon_tracking_web/app/add_user.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from passlib.context import CryptContext
 
 # Setup MongoDB connection (USE YOUR ACTUAL URI BELOW)
-client = MongoClient("mongodb+srv://bhanujasharma2223:bRh51nQwd9UooE5C@majorproject.elha1mu.mongodb.net/?retryWrites=true&w=majority&appName=MajorProject")
+client = MongoClient("YOUR CONNECTION STRING")
 db = client["ai_recon_tracking"]
 users_col = db["users"]
 
